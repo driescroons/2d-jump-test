42e5bb95449de90170514f3cff5670ed4a11ed2e
Dries Croons <dries@phantom.app>
2023-07-16 03:47:07 +0200

    level

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,124 @@ function App() {
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Physics debug>
-          <Player position={[-3, 1, 0]} />
+          <Player position={[0, 1, 0]} />
 
           {/* base */}
           <Platform args={[100, 1, 1]} position={[0, 0, 0]} />
 
-          {/* high blocks */}
-          <Platform args={[1, 2, 1]} position={[-5, 1.5, 0]} color="green" />
-          <Platform args={[1, 3, 1]} position={[-10, 2, 0]} color="yellow" />
+          {/* left jumping blocks */}
+          {[...Array(6)].map((_, i) => (
+            <Platform
+              args={[2, 1, 1]}
+              position={[-5 - i * (3 + 0.5 * i), 2, 0]}
+              color={i >= 4 ? "orange" : i >= 2 ? "yellow" : "green"}
+            />
+          ))}
+
+          {/* wall jumping blocks */}
+          {[...Array(6)].map((_, i) => (
+            <>
+              <Platform
+                args={[1, 10, 1]}
+                position={[2.5 + i / 2, 10 + 10 * i, 0]}
+                color="grey"
+              />
+              <Platform
+                args={[1, 10, 1]}
+                position={[-2.5 - i / 2, 10 + 10 * i, 0]}
+                color="grey"
+              />
+            </>
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[5, 1, 1]}
+              position={[-10 - i * 6, 8 + i * 2, 0]}
+              color="green"
+            />
+          ))}
+
+          <Platform args={[1, 10, 1]} position={[-28, 18, 0]} />
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[5, 1, 1]}
+              position={[-22 + i * 6, 18 + i * 3, 0]}
+              color="yellow"
+            />
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[5, 1, 1]}
+              position={[-10 - i * 6, 29 + i * 4, 0]}
+              color="orange"
+            />
+          ))}
+
+          <Platform args={[1, 10, 1]} position={[-28, 40, 0]} />
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[5, 1, 1]}
+              position={[-22 + i * 6, 43 + i * 5, 0]}
+              color="red"
+            />
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[2, 1, 1]}
+              position={[7 + i * 5, 8 + i * 2, 0]}
+              color="yellow"
+            />
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[2, 1, 1]}
+              position={[20 - i * 6, 17 + i * 2, 0]}
+              color="yellow"
+            />
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[2, 1, 1]}
+              position={[10 + i * 6, 26 + i * 2.5, 0]}
+              color="yellow"
+            />
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[2, 1, 1]}
+              position={[20 - i * 6, 35 + i * 2.5, 0]}
+              color="yellow"
+            />
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[2, 1, 1]}
+              position={[10 + i * 6, 45 + i * 3, 0]}
+              color="yellow"
+            />
+          ))}
+
+          {[...Array(3)].map((_, i) => (
+            <Platform
+              args={[2, 1, 1]}
+              position={[20 - i * 6.5, 55 + i * 3, 0]}
+              color="yellow"
+            />
+          ))}
+
+          {/* <Platform args={[3, 1, 1]} position={[-4, 2, 0]} color="green" />
+          <Platform args={[3, 1, 1]} position={[-10, 3, 0]} color="yellow" />
           <Platform args={[1, 4, 1]} position={[-15, 2.5, 0]} color="orange" />
-          <Platform args={[1, 5, 1]} position={[-20, 3, 0]} color="red" />
+          <Platform args={[1, 5, 1]} position={[-20, 3, 0]} color="red" /> */}
 
           {/* platforms */}
           <Platform args={[10, 2, 1]} position={[10, 1.5, 0]} color="green" />
@@ -41,11 +149,6 @@ function App() {
           <Platform args={[10, 2, 1]} position={[70, 22.5, 0]} color="red" />
 
           {/* platforms up top */}
-          <Platform args={[4, 1, 1]} position={[11, 6, 0]} />
-          <Platform args={[4, 1, 1]} position={[7, 7, 0]} />
-          <Platform args={[4, 1, 1]} position={[3, 8, 0]} />
-          <Platform args={[4, 1, 1]} position={[-1, 9, 0]} />
-          <Platform args={[4, 1, 1]} position={[-5, 10, 0]} />
         </Physics>
       </Canvas>
     </>
